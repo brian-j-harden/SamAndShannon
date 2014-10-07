d4a268fd0013bb6ac0128e339151b8603ffdd98b
brian-j-harden <Brian.J.Harden@gmail.com>
2014-10-06 21:37:49 -0400

    images are now actual size

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,41 @@
 /**
  * Created by bharden on 9/26/14.
  */
+//var fs = require('fs');
 
-var imageSet1 = ["2014-02-11 19.15.59.jpg", "2014-02-11 19.17.13.jpg", "2014-02-13 19.54.53.jpg"];
-var currentImageSet1 = 0;
-
-var imageSet2 = ["2014-02-14 17.27.25.jpg", "2014-02-14 18.05.47.jpg", "2014-02-14 19.45.08.jpg"];
-var currentImageSet2 = 0;
+var imageSet = ["2014-02-11 19.15.59.jpg", "2014-02-11 19.17.13.jpg", "2014-02-13 19.54.53.jpg", "2014-02-14 17.27.25.jpg", "2014-02-14 18.05.47.jpg", "2014-02-14 19.45.08.jpg"];
+var imageIndex = -1;
 
 function changeBackgroundImages() {
-    img1Fade();
-    setTimeout(img2Fade, 2000);
+    imgFade('#bImg1', '#bImg2');
+    setTimeout(function(){imgFade('#bImg2', '#bImg1')}, 4000);
+}
+
+function imgFade(divId1, divId2) {
+
+    // Get a random number for the next index, but don't let it be the same as the previous value
+    tmpIndex = imageIndex;
+    imageIndex = getRandomInteger(0, imageSet.length);
+    while (tmpIndex == imageIndex) {
+        imageIndex = getRandomInteger(0, imageSet.length);
+    }
+
+    console.log('Index: ' + imageIndex);
+
+    // fade in the backup background
+    $(divId2).fadeIn(2500);
+
+    // fade out the existing background and change the image once the fadeout is complete, to get ready for the next time
+    $(divId1).fadeOut(2500, function(){
+        $(divId1).css("background", "url(\"/img/" + imageSet[imageIndex] + "\") no-repeat");
+        $(divId1).css("background-size", "contain");
+//        background-size: contain;
+
+    });
 
 }
 
-function img1Fade(){
-
-    $('#bImg1').fadeOut('slow', function(){$('#bImg1').css("background", "url(\"/img/" + imageSet1[++currentImageSet1] + "\") no-repeat")});
-    $('#bImg2').fadeIn('slow');
-    console.log('img1 background: '+ $('#bImg1').css("background"));
-    console.log('currentImageSet1 is '+currentImageSet1);
-    console.log('image1 is '+imageSet1[currentImageSet1]);
-    if (currentImageSet1 >= imageSet1.length - 1) {
-
-        currentImageSet1 -= imageSet1.length;
-        console.log('reset currentImageSet1 to ' + currentImageSet1);
-    };
-}
-
-function img2Fade(){
-
-    $('#bImg2').fadeOut('slow', function(){$('#bImg2').css("background", "url(\"/img/" + imageSet2[++currentImageSet2] + "\") no-repeat")});
-    $('#bImg1').fadeIn('slow');
-    console.log('currentImageSet2 is '+currentImageSet2);
-    console.log('image2 is '+imageSet2[currentImageSet2]);
-    if (currentImageSet2 >= imageSet2.length - 1) {
-
-        currentImageSet2 -= imageSet2.length;
-        console.log('reset currentImageSet2 to ' + currentImageSet2);
-    };
-}
 
 $(document).ready(function(){
-
-    setInterval(changeBackgroundImages, 4000);
+    setInterval(changeBackgroundImages, 8000);
 });
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,3 +1,8 @@
 /**
  * Created by bharden on 10/1/14.
  */
+
+function getRandomInteger(min, max) {
+    // Returns a random integer between min (included) and max (excluded)
+    return Math.floor(Math.random() * (max - min)) + min;
+}
